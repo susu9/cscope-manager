3755890f5e1734d77d5a9e08ef415f600637a66f
Rick Chang <rick@Ricks-MacBook-Air.local>
2021-01-01 14:14:43 -0800

    Support include specific file without suffix

--- test/test.sh
+++ test/test.sh
@@ -5,23 +5,23 @@ checkDelete() {
     files=$1
     for file in $files; do
         if [ -e $file ]; then
-            assertNotEquals 0 $? 
+            assertNotEquals 0 $?
         fi
     done
 }
 
 testBasic() {
     python csmgr -h
-    assertEquals 0 $? 
+    assertEquals 0 $?
     python csmgr -v
-    assertEquals 0 $? 
+    assertEquals 0 $?
 }
 
 testFile() {
     python csmgr test/file1.c
-    assertEquals 0 $? 
+    assertEquals 0 $?
     grep -w test/file1.c cscope.files
-    assertEquals 0 $? 
+    assertEquals 0 $?
     python csmgr -d
     checkDelete "cscope.in.out cscope.out cscope.po.out tags"
     rm .csmgr.project
@@ -29,48 +29,48 @@ testFile() {
 
 testForce() {
     python csmgr test/file1.c
-    assertEquals 0 $? 
+    assertEquals 0 $?
     python csmgr -f
-    assertEquals 0 $? 
+    assertEquals 0 $?
     python csmgr -d
     rm .csmgr.project
 }
 
 testDir() {
     python csmgr test/dir
-    assertEquals 0 $? 
+    assertEquals 0 $?
     grep -w test/dir/dir_file1.c cscope.files
-    assertEquals 0 $? 
+    assertEquals 0 $?
     grep -w test/dir/dir_file2.c cscope.files
-    assertEquals 0 $? 
+    assertEquals 0 $?
     python csmgr -d
     rm .csmgr.project
 }
 
 testSuffix() {
     python csmgr . -s .cpp
-    assertEquals 0 $? 
+    assertEquals 0 $?
     grep -w test/dir/dir_file1.c cscope.files
-    assertNotEquals 0 $? 
+    assertNotEquals 0 $?
     grep -w test/dir/dir_file2.c cscope.files
-    assertNotEquals 0 $? 
+    assertNotEquals 0 $?
     grep -w test/file2.cpp cscope.files
-    assertEquals 0 $? 
+    assertEquals 0 $?
     python csmgr -d
     rm .csmgr.project
 }
 
 testList() {
-    python csmgr test/file1.c -o src.map 
-    assertEquals 0 $? 
+    python csmgr test/file1.c -o src.map
+    assertEquals 0 $?
     grep -w test/file1.c src.map
     python csmgr -d
-    rm .csmgr.project
+    rm .csmgr.project src.map
 }
 
 testMeta() {
-    python csmgr . 
-    assertEquals 0 $? 
+    python csmgr .
+    assertEquals 0 $?
     python csmgr -d -m cscope.files cscope.in.out cscope.out cscope.po.out tags .csmgr.project
     checkDelete "cscope.in.out cscope.out cscope.po.out tags .csmgr.project"
 }
@@ -78,11 +78,11 @@ testMeta() {
 testCmd() {
     python csmgr test/file1.c -x 'echo "test/dir/dir_file1.c"' 'echo "test/dir/dir_file2.c"' > cmd.out
     grep -w test/dir/dir_file1.c cmd.out
-    assertEquals 0 $? 
+    assertEquals 0 $?
     grep -w test/dir/dir_file2.c cmd.out
-    assertEquals 0 $? 
+    assertEquals 0 $?
     python csmgr -d
-    rm .csmgr.project
+    rm .csmgr.project cmd.out
 }
 
 CONFIG_FILE='test/.csmgr.config'
@@ -90,22 +90,22 @@ CONFIG_FILE='test/.csmgr.config'
 testExclude() {
     python csmgr -c $CONFIG_FILE  test -e dir/
     grep -w test/dir/dir_file1.c cscope.files
-    assertNotEquals 0 $? 
+    assertNotEquals 0 $?
     grep -w test/dir/dir_file2.c cscope.files
-    assertNotEquals 0 $? 
+    assertNotEquals 0 $?
     python csmgr -c $CONFIG_FILE -d
     rm .csmgr.project
 }
 
 testSuffix2() {
     python csmgr -c $CONFIG_FILE .
-    assertEquals 0 $? 
+    assertEquals 0 $?
     grep -w test/dir/dir_file1.c map.files
-    assertNotEquals 0 $? 
+    assertNotEquals 0 $?
     grep -w test/dir/dir_file2.c map.files
-    assertNotEquals 0 $? 
+    assertNotEquals 0 $?
     grep -w test/file2.cpp map.files
-    assertEquals 0 $? 
+    assertEquals 0 $?
     python csmgr -c $CONFIG_FILE -d
     rm .csmgr.project
 }
@@ -113,10 +113,31 @@ testSuffix2() {
 testExclude2() {
     python csmgr -c $CONFIG_FILE test
     grep -w test/dir/dir_file1.c map.files
-    assertNotEquals 0 $? 
+    assertNotEquals 0 $?
     grep -w test/dir/dir_file2.c map.files
-    assertNotEquals 0 $? 
+    assertNotEquals 0 $?
     python csmgr -c $CONFIG_FILE -d
     rm .csmgr.project
 }
 
+CONFIG_FILE='test/.csmgr.include.config'
+
+testInculude1() {
+    python csmgr -c $CONFIG_FILE test
+    grep -w TARGETS cscope.files
+    assertEquals 0 $?
+    grep -w Makefile cscope.files
+    assertEquals 0 $?
+    python csmgr -d
+    rm .csmgr.project
+}
+
+testInculude2() {
+    python csmgr test -i TARGETS Makefile
+    grep -w TARGETS cscope.files
+    assertEquals 0 $?
+    grep -w Makefile cscope.files
+    assertEquals 0 $?
+    python csmgr -d
+    rm .csmgr.project
+}
